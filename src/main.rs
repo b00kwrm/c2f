b162d014a24fa0383a5f4f42ee665a5e9e9c6f26
dave <dave@harpo.marx>
2019-05-27 07:25:47 -0400

    add user input

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
+use std::io;
+
 fn main() {
-    let f = (0 * 9/5) + 32;
-    println!("0 degrees Celsius is {} degree Farenheit", f);
+    println!("How many degrees Celsius?");
+
+    let mut celsius_degrees = String::new();
+
+    io::stdin().read_line(&mut celsius_degrees)
+        .expect("failed to read line");
+
+    let celsius_degrees: u32 = celsius_degrees.trim().parse().expect("please type a number");
+
+        
+    let f = (celsius_degrees * 9/5) + 32;
+    println!("{} degrees Celsius is {} degree Farenheit", celsius_degrees, f);
 }
